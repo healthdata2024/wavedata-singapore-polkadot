8e32d5c0341f5e5cca33ccfd68d8cbe980d9ef06
healthdata2024 <wavedata.info@gmail.com>
2024-08-22 23:21:10 +0600

    codes

---
+++ wavedata/src/services/getContract.js
@@ -0,0 +1,20 @@
+
+import { Abi, ContractPromise } from '@polkadot/api-contract'
+
+import { ApiPromise, Keyring, WsProvider } from '@polkadot/api'
+import abiData from '../ink_contracts/wavedata/target/ink/metadata.json';
+import { options } from '@astar-network/astar-api';
+
+
+const address = 'Zxnt9BCBjhWXdNbHizbbZAM7W3UdhKfXj16GAo3PNzQDfd6'//smart contract deployed address 
+	
+export default async function getContract(api) {
+
+
+    const abi = new Abi(abiData, api.registry.getChainProperties())
+
+    const contract = new ContractPromise(api, abi, address)
+
+	return contract
+  }
+  
